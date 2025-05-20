8011afa6e06b017d19825b3fc6155b50b3b6757b
kaiunix17 <ismoilovshohabbos25@gmail.com>
2025-05-20 15:14:57 +0500

    change app.js bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3050;
 
 // Serve static files from the "views" directory
 app.use(express.static('views'));
